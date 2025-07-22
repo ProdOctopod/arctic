900de70bf6253573bb7332e87820f5abbb2761e6
Jason Andre <jason@octopod.au>
2025-07-22 12:06:49 +1000

    Fix typo.

--- tests/integration/store/test_pandas_store.py
+++ tests/integration/store/test_pandas_store.py
@@ -943,7 +943,7 @@ def test_daterange_when_end_beyond_chunk_index_no_start(library):
                               2015-08-10 00:00:00,200012,2.0
                               2015-08-10 00:00:00,200016,3.0
                               2015-08-11 00:00:00,200005,1.0
-                              2015-08-11 00:00:00,200012,2,0
+                              2015-08-11 00:00:00,200012,2.0
                               2015-08-11 00:00:00,200016,3.0"""), parse_dates=[0],
                   names=['date', 'security_id', 'value']).set_index(['date', 'security_id'])
     library.write('MYARR', df)
